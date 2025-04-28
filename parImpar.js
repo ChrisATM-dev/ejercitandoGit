a678cb3a479b0fb498adf178129622c6393c68b2
Christian Torres <Christian.atm24@gmail.com>
2025-04-28 16:35:41 -0400

    agregue la funcion esPar

--- parImpar.js
+++ parImpar.js
@@ -1,3 +1,9 @@
 // ver si es par un numero
-
+const esPar = (num) => {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }    
+}
 // ver si es impar un numero
